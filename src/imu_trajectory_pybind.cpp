22b9bec985f01b71708b18298a5d5c17418b282c
Sandeep Menon <sandeepmenon@deepen.ai>
2022-05-12 18:06:04 +0530

    1. reordered imu trajcetory utils 2. added pybind11 module

---
+++ src/imu_trajectory_pybind.cpp
@@ -0,0 +1,16 @@
+#include <pybind11/pybind11.h>
+#include <imu_trajectory_utils.cpp>
+namespace py = pybind11;
+
+string TestBinding() {
+    return "Hello binders!";
+}
+
+PYBIND11_MODULE(py_imu_trajectory, module) {
+    module.doc() = "pybind11 plugin for calculating trajectory of IMU using IMU readings.";
+
+    module.def("test_binding", &TestBinding, "Test Binding Function");
+    module.def("get_imu_ahrs_output", &get_imu_ahrs_output, 
+    "get_imu_ahrs_output", 
+     py::arg("file_path"), py::arg("num_rows"), py::arg("sampleFrequency"), py::arg("useEuler"));
+}
---
+++ src/imu_trajectory_utils.hpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+// Returns IMU readings of the form <timestamp, gx, gy, gz, ax, ay, az>
+vector<vector<double>> read_IMU_from_csv(const string file_path, const string delimiter, const int num_rows);
+
+// Write IMU readings to a csv file
+void write_IMU_to_csv(const string file_path, const string delimiter, const vector<vector<double>> data, const vector<string> header);
+
+// Convert radians to degrees
+double convert_radians_to_degree(double angle);
+
+// Returns IMU readings of the form <timestamp, qw, qx, qy, qz> or <timestamp, roll, pitch, yaw>
+vector<vector<double>> get_imu_ahrs_output(string file_path, const int num_rows, const int sampleFrequency, const int useEuler);
